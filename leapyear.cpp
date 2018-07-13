7bf63e0f09baa8edba84616dda362d7b54a07743
manandeep123 <40437004+manandeep123@users.noreply.github.com>
2018-07-13 15:32:14 +0530

    Add files via upload

---
+++ leapyear.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter a year";
+cin>>n;
+if(n%4==0&&n>0)
+{cout<<"it is a leap year";}
+
+else
+{ cout<<"it is not a leap year";
+}
+
+}
+
